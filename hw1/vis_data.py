cd50dd6a79e5d49b75d76c273f2b36072241ef48
wenyuan.chi <wenyuan.chi@ileopard.com>
2017-03-16 14:54:34 +0800

    visualize data

---
+++ hw1/vis_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./data/train.csv', na_values='NR', encoding='big5')
+
+days = ['2014/1/1', '2014/1/2', '2014/1/3']
+factors = df[u'測項'].unique()
+
+frames = []
+for day in days:
+	dfByDay = df[df[u'日期'].isin([day])].iloc[:, 3:]
+	dfByDay.index = factors
+	frames.append(dfByDay)
+
+training = pd.concat(frames, axis = 1)
+training = training.T
+training.index = pd.DatetimeIndex(start=days[0], freq='H', periods=len(days)*24)
+
+ax = training.plot(linestyle='', marker='o', y='PM2.5', label='PM2.5')
+training.plot(linestyle='', marker='o', ax = ax, y='PM10', label='PM10')
+
+training.plot(kind="scatter", x="PM2.5", y="PM10")
+training.plot(kind="scatter", x="PM2.5", y="WIND_DIREC")
+plt.show()
